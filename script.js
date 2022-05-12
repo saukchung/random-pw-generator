65ed38458a9db636411f2c5eb9711c1a686665b4
Sa Uk Chung <chungsauk95@gmail.com>
2022-05-12 16:36:04 -0400

    When the button is pressed, the user is prompted to type in a preferred length between 8 and 128. If it isn't a valid number, it alerts the user and returns to the first question.

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 // Assignment code here
 
 
+// Function for criterias
+function crits() {
+
+  // Prompts user for the criteria
+  var crit1 = prompt("Choose the length of your password(8 ~ 128)")
+
+    //  If the crit1 is between 8 ~ 128
+  if (crit1 < 128 && crit1 > 8) {
+    var pwLength = crit1
+    return pwLength
+  }
+
+    //  If the crit1 is not between 8 ~ 128 (Including strings)
+    //    Alerts and returns to the beginning
+  else {
+    alert("Has to be a number between 8 and 128...");
+    return crits()
+  }
+
+    //  Criterion 2
+  var crit2 = prompt("What character types do you want in your password?")
+
+
+}
+
+function generatePassword() {
+ crits();
+
+}
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -14,4 +44,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+//generateBtn.addEventListener("click", writePassword);
 generateBtn.addEventListener("click", writePassword);
