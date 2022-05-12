b89a5d0455ca0ea3a165925f8b3a5b697fe15bed
Sa Uk Chung <chungsauk95@gmail.com>
2022-05-12 17:27:51 -0400

    Added a scrond criterion as a function. Now it prompts the user 4 times as a confirm() to prompt the inclusion of 4 types of characters. Generating a listPrefChar that contains all the user preferred characters.

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 
 
 // Function for criterias
-function crits() {
+function criterion1() {
 
   // Prompts user for the criteria
-  var crit1 = prompt("Choose the length of your password(8 ~ 128)")
+  var crit1 = prompt("Choose the length of your password", "Any number between 8 ~ 128")
 
     //  If the crit1 is between 8 ~ 128
   if (crit1 < 128 && crit1 > 8) {
@@ -17,19 +17,48 @@ function crits() {
     //    Alerts and returns to the beginning
   else {
     alert("Has to be a number between 8 and 128...");
-    return crits()
+    return criterion1()
   }
+}
+  
+//  Criterion 2
+function criterion2() {
+  var listPrefChar = []
+  var crit21 = confirm("Include lowercase?")
+  if (crit21 === true) {
+    var allLowerCase = "abcdefghijklmnopqrstuvwxyz".split("")
+    listPrefChar = listPrefChar.concat(allLowerCase)
+    }
+  var crit22 = confirm("Include uppercase?")
+  if (crit22 === true) {
+    var allUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+    listPrefChar = listPrefChar.concat(allUpperCase)
+    }
 
-    //  Criterion 2
-  var crit2 = prompt("What character types do you want in your password?")
+  var crit23 = confirm("Include numbers?")
+  if (crit23 === true) {
+    var allNumbers = "1234567890".split("")
+    listPrefChar = listPrefChar.concat(allNumbers)
+    }  
 
+  var crit24 = confirm("Include special characters?")
+  if (crit24 === true) {
+    var allSpecial = "!$%&'()*+,-./:;<=>?@[\]^_`{|}~ ".split("")
+    listPrefChar = listPrefChar.concat(allSpecial, '"')
+    }
 
+  return listPrefChar;
+  
 }
 
-function generatePassword() {
- crits();
 
-}
+
+
+// function generatePassword() {
+//  for (i = 0criterion1()
+//  console.log(criterion2());
+
+// }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
